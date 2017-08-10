26f67c5dde94976d7130b1885d535753cceb848c
Romain Picard <romain.picard@oakbits.com>
2017-08-10 22:57:31 +0200

    es2015: changed test structure, added more tests on item creation

--- test/es2015/base_type/test.js
+++ test/es2015/base_type/test.js
@@ -1,8 +1,30 @@
-import {Simple1} from './lib/base_type.js';
+import {SingleField, MultiFields} from './base_type.rxt.js';
 var assert = require('assert');
 
 describe('creation of', function() {
-  describe('Simple1 object', function() {
+  describe('SingleField object', function() {
+    it('should return an object with the provided parameters', function() {
+      let expectedItem = {
+        "foo32": 42
+      };
+      let item = SingleField(42);
+      assert.deepEqual(item, expectedItem);
+    });
+  });
+});
+
+describe('creation of', function() {
+  describe('SingleField object', function() {
+    it('should fail when field count is not correct', function() {
+      assert.throws( () => { return SingleField(); }, Error);
+      assert.throws( () => { return SingleField(1, 2); }, Error);
+    });
+  });
+});
+
+
+describe('creation of', function() {
+  describe('MultiFields object', function() {
     it('should return an object with the provided parameters', function() {
       let expectedItem = {
         "foo32": 1,
@@ -10,10 +32,20 @@ describe('creation of', function() {
         "foo64": 3,
         "bar64": 4,
         "biz": true,
-        "buz": 1.2
+        "buz": 1.2,
+        "name": "baz"
       };
-      let item = Simple1(1 ,2 ,3 ,4 ,true ,1.2);
+      let item = MultiFields(1 ,2 ,3 ,4 ,true ,1.2, "baz");
       assert.deepEqual(item, expectedItem);
+    });
+  });
+});
+
+describe('creation of', function() {
+  describe('MultiFields object', function() {
+    it('should fail when field count is not correct', function() {
+      assert.throws( () => { return MultiFields(1, 2 ,3 ,4 ,true ,1.2); }, Error);
+      assert.throws( () => { return MultiFields(1, 2 ,3 ,4 ,true ,1.2, "me", "you"); }, Error);
     });
   });
 });
